ad725fec4a64847205014e1e1a21f0dd80a01e08
afsanehpeymani-1373 <120179013+afsanehpeymani-1373@users.noreply.github.com>
2023-01-08 15:57:09 +0330

    Initial commit

---
+++ tamrin 2-1.py
@@ -0,0 +1,16 @@
+# tamrin 2-1
+# dar mored print()
+print("Day 1 - Setting Mainpulation")
+print("-------------------------------")
+
+print('String Concatetion is  done with the'  " + "' sign')
+print("-------------------------------")
+
+print('e.g print ("Hello" + "word" )')
+print("-------------------------------")
+
+print('New lines can be created with a backslash and n.')
+print("-------------------------------")
+
+
+
---
+++ tamrin 2-3.py
@@ -0,0 +1,7 @@
+# tamrin 2-3
+# dar mored variable and print
+
+print("Congratulations for the program ")
+your_city = input("In which city did you grow up? ")
+your_pet = input("What is your pet's name? ")
+print(f" your_city is {your_city} and your_pet is {your_pet}")
